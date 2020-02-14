8f8274dae67a5c8909e8d05343bde1f0e7dd27fe
NinaMiroshnychenko <nina.m@medichi.com.ua>
2020-02-14 15:25:48 +0200

    Updates

---
+++ precache-manifest.8178e9052be2858ca26a0f178d176f4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb900b8a5026e3d270e6af0057d4f015",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "be1dc80d0c8849976879",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.b3f82fd1.chunk.css"
+  },
+  {
+    "revision": "87118d6f4f175dc8f454",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.88bb62e2.chunk.css"
+  },
+  {
+    "revision": "be1dc80d0c8849976879",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b0b2f8ec.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b0b2f8ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87118d6f4f175dc8f454",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.b1351fe7.chunk.js"
+  },
+  {
+    "revision": "9ed00262ab067fcfa080",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.5a6095d3.js"
+  }
+]);
