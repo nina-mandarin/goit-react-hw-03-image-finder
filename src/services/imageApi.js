348fef69733ba72dfa5800d29d6edaa427742255
NinaMiroshnychenko <nina.m@medichi.com.ua>
2020-02-14 15:24:30 +0200

    Fixed imageApi

--- src/services/imageApi.js
+++ src/services/imageApi.js
@@ -3,23 +3,10 @@ import axios from 'axios';
 const baseUrl = 'https://pixabay.com/api/';
 const key = '14230040-8f4217119aa6e0fd5b0363ac7';
 
-const params = {
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
-  }
-};
-
-// const getImages = (query, page) => {
-//   const url = `https://cors-anywhere.herokuapp.com/${baseUrl}?key=${key}&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`;
-
-//   return axios.get(url, params).then(response => response.data);
-// }
-
 const getImages = async (query, page) => {
-  const url = `https://cors-anywhere.herokuapp.com/${baseUrl}?key=${key}&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`;
+  const url = `${baseUrl}?key=${key}&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`;
 
-  const response = await axios.get(url, params);
+  const response = await axios.get(url);
   const data = response.data;
 
   return data;
